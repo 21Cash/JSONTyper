44f5a31456e9fb9c9ca85ff5c1f24cfc3bbd79f2
21ThousandProof <21ThousandProof.@gmail.com>
2023-06-09 17:16:33 +0530

    Added New and Improved Build and source files

--- src/JsonExtracter.py
+++ src/JsonExtracter.py
@@ -1,11 +1,11 @@
 import json
 import pyautogui
-import time
+# import time
 
-delayBeforeStart = 5
+# delayBeforeStart = 5
 
 logObj = open("log.txt", "w")
-outputFile = open("code.txt", "w")
+outputFile = open("input.txt", "w")
 
 def log(str):
 	logObj.write(str)
@@ -19,15 +19,16 @@ data = json.loads(jsonString)
 code = data['ref']['solution']['filesContentArr'][0]['fileContent']
 
 outputFile.write(code)
-time.sleep(delayBeforeStart)
+
+# time.sleep(delayBeforeStart)
 
 pyautogui.PAUSE = 0.0001
 
 
-for line in code:
-	for ch in line:
-		if ch != '\t':
-		    pyautogui.write(ch)
+# for line in code:
+# 	for ch in line:
+# 		if ch != '\t':
+# 		    pyautogui.write(ch)
 		    
 log("Writing Completed")
 logObj.close()
